9ad91d3c1c0f8224380dae0c4dba675b9df49ef5
tt404184768 <371525279@qq.com>
2023-08-06 08:31:20 +0800

    sql

--- src/main/java/com/example/azuredemo/controller/IndexController.java
+++ src/main/java/com/example/azuredemo/controller/IndexController.java
@@ -1,7 +1,13 @@
 package com.example.azuredemo.controller;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  * 默认控制器
@@ -19,4 +25,30 @@ public class IndexController {
         return "azure-demo-git::2305";
     }
 
+    @GetMapping("/demoMSI")
+    @ResponseBody
+    public String demoMSI(){
+        String result = "";
+        SQLServerDataSource dataSource = new SQLServerDataSource();
+        // Replace with your server name
+        //demo-rg-sqldb-server.database.windows.net
+        dataSource.setServerName("demo-rg-sqldb-server.database.windows.net");
+        // Replace with your database name
+        dataSource.setDatabaseName("demo-re-sqldb");
+        dataSource.setAuthentication("ActiveDirectoryManagedIdentity");
+        try{
+            Connection connection = dataSource.getConnection();
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT SUSER_SNAME()");
+            if (rs.next()) {
+                result = "You have successfully logged on as: " + rs.getString(1);
+                System.out.println(result);
+            }
+        }catch (Exception e){
+
+        }
+        return result;
+    }
 }
+
+
