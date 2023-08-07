e7e41944e7941cd84cea68a148982303f9b243a7
tt404184768 <371525279@qq.com>
2023-08-07 22:54:14 +0800

    dataSource.setURL

--- src/main/java/com/example/azuredemo/controller/IndexController.java
+++ src/main/java/com/example/azuredemo/controller/IndexController.java
@@ -22,7 +22,7 @@ public class IndexController {
      */
     @GetMapping
     public String index(){
-        return "azure-demo-git::0806::0831";
+        return "azure-demo-git::0807::2253";
     }
 
     @GetMapping("/demoMSI")
@@ -36,12 +36,41 @@ public class IndexController {
         // Replace with your database name
         dataSource.setDatabaseName("demo-re-sqldb");
         dataSource.setAuthentication("ActiveDirectoryManagedIdentity");
+
+//        dataSource.setMSIClientId("");
         try{
             Connection connection = dataSource.getConnection();
             Statement stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT SUSER_SNAME()");
             if (rs.next()) {
-                result = "You have successfully logged on as: " + rs.getString(1);
+                result = "demoMSI::You have successfully logged on as: " + rs.getString(1);
+                System.out.println(result);
+            }
+        }catch (Exception e){
+            result="连接异常："+e.getMessage();
+
+        }
+        return result;
+    }
+
+    @GetMapping("/demoMSI1")
+    @ResponseBody
+    public String demoMSI1(){
+        String result = "";
+        SQLServerDataSource dataSource = new SQLServerDataSource();
+        // Replace with your server name
+        //demo-rg-sqldb-server.database.windows.net
+//        dataSource.setServerName("demo-rg-sqldb-server.database.windows.net");
+        // Replace with your database name
+//        dataSource.setDatabaseName("demo-re-sqldb");
+//        dataSource.setAuthentication("ActiveDirectoryManagedIdentity");
+        dataSource.setURL("jdbc:sqlserver://demo-rg-sqldb-server.database.windows.net:1433;database=demo-re-sqldb;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;Authentication=ActiveDirectoryIntegrated");
+        try{
+            Connection connection = dataSource.getConnection();
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT SUSER_SNAME()");
+            if (rs.next()) {
+                result = "demoMSI1::You have successfully logged on as: " + rs.getString(1);
                 System.out.println(result);
             }
         }catch (Exception e){
